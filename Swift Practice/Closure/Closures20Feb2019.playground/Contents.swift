e38920cc52e336026e3faddae47adb2eefa71ab8
Chaman Gurjar <chamangujjar162@gmail.com>
2019-02-20 22:15:46 +0530

    Various types of writing a closure in swift

--- Swift Practice/Closure/Closures20Feb2019.playground/Contents.swift
+++ Swift Practice/Closure/Closures20Feb2019.playground/Contents.swift
@@ -1,3 +1,40 @@
 import UIKit
 
-var str = "Hello, playground"
+/// Closure type 1
+var closureOne : (Int, Int) -> Int = { (numberOne: Int, numberTwo: Int) in
+    return numberOne * numberTwo
+}
+
+closureOne(3, 5)
+
+/// Closure type 2  - Without Explicit Type Declaration
+var closureTwo  = {
+    (numberOne: Int, numberTwo: Int) in
+    return numberOne * numberTwo
+}
+
+closureTwo(4, 6)
+
+/// Closure type 3  - Without return keyword
+var closureThree  = { (numberOne: Int, numberTwo: Int) in
+    numberOne * numberTwo
+}
+
+closureThree(5, 7)
+
+/// Closure type 4
+var closureFour: (Int, Int) -> Int  = { $0 * $1 }
+closureFour(6, 8)
+
+
+var closureOneWithString: () -> String = { () in
+    print("If you are using return keyword in closure, you can also add print statement.")
+    return "Hi this is simple print statement with closure one."
+}
+var closureTwoWithString: () -> String = { () in
+    "Hi this is simple print statement with closure two."
+}
+var closureThreeWithString: () -> String = { "Hi this is simple print statement with closure three." }
+var closureFourWithString = { "Hi this is simple print statement with closure four." }
+closureOneWithString()
+print(closureFourWithString())
