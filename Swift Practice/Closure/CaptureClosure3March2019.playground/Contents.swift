7ead727c6cdc019988a9ff35293be63d289a43a3
Chaman Gurjar <chamangujjar162@gmail.com>
2019-03-03 17:27:14 +0530

    Capture Value in closure.

---
+++ Swift Practice/Closure/CaptureClosure3March2019.playground/Contents.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+/*
+ Closures Are Refrence Type
+ */
+
+var closureArray : [() -> ()] = []
+var i = 0;
+
+for _ in 1...5 {
+    closureArray.append {
+        print(i)
+    }
+    i = i + 1;
+}
+
+closureArray[0]()
+
+// Although i'm passing 0 but it is printing 5 because it is holding reference of i and at last i = 5 at line no 14. So it is printing 5.
+
+
+//******************************************************************************************//
+// In this way we can capture the value.
+
+var otherClosureArray: [() -> ()] = []
+var j = 0
+
+for _ in 1...5 {
+    otherClosureArray.append { [j] in
+        print(j)
+    }
+    j = j + 1
+}
+
+otherClosureArray[1]()
+otherClosureArray[2]()
+otherClosureArray[3]()
+otherClosureArray[4]()
+
