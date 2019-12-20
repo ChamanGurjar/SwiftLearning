1a12735d2395f2ead71095786817a4dbd249a999
ChamanGurjar <chamangujjar162@gmail.com>
2019-12-20 08:31:32 +0530

    Converted to swift 5

---
+++ Swift Practice/Practice.swift
@@ -0,0 +1,18 @@
+//
+//  Practice.swift
+//  Swift Practice
+//
+//  Created by ChamanGurjar on 20/12/19.
+//  Copyright © 2019 Chaman Gurjar. All rights reserved.
+//
+
+import Foundation
+
+class Practice {
+    
+    var name: String?
+    
+    init(name: String?) {
+        self.name = name
+    }
+}
--- Swift Practice/ViewController.swift
+++ Swift Practice/ViewController.swift
@@ -9,10 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var practice = Practice(name: "Chaman")
+        print("Practice Name : \(practice.name ?? nil)")
+        
+        var anotherPractice = practice
+        anotherPractice.name = "Abhijeet"
+        print("Another Practice Name : \(anotherPractice.name ?? nil)")
+        print("Practice Name : \(practice.name ?? nil)")
+        
+        
+        print("After Setting Nil")
+        anotherPractice.name = nil
+        print("Another Practice Name : \(anotherPractice.name)")
+        print("Practice Name : \(practice.name)")
+        
     }
 
 
