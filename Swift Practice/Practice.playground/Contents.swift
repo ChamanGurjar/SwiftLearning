a12b3a54d25b226e99715413378f7319d50ed162
ChamanGurjar <chamangujjar162@gmail.com>
2019-12-25 18:07:14 +0530

    Binary Operator

--- Swift Practice/Practice.playground/Contents.swift
+++ Swift Practice/Practice.playground/Contents.swift
@@ -1,3 +1,9 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//MARK :- Operator
+var strConcatenationOne = str + ", Its Chaman Here."
+//var strConcatenationOne = str + 2 // it gaves error => error: binary operator '+' cannot be applied to operands of type 'String' and 'Int'
+
+
